9458e0ec5e76063cf31dd71b81bd71dd65d95159
Kristyna Kupkova <kristynakupkova@gmail.com>
2023-09-19 15:59:21 -0400

    remove non-ASCII character to avoid warning

--- data-raw/markerDatabase.R
+++ data-raw/markerDatabase.R
@@ -1,4 +1,6 @@
 library(usethis)
 markerDatabase = read.delim("https://raw.githubusercontent.com/kkupkova/Mouse-bone-markers/main/BONE_DATABASE.tsv")
 markerDatabase = markerDatabase[, !(names(markerDatabase) == "type")]
+# encode greek letters to UTF-8
+markerDatabase$gene = enc2utf8(markerDatabase$gene)
 usethis::use_data(markerDatabase, overwrite = TRUE)
--- data-raw/marker_lists.R
+++ data-raw/marker_lists.R
@@ -2,16 +2,24 @@ library(usethis)
 
 # cellType_list
 load(url("https://github.com/kkupkova/Mouse-bone-markers/raw/main/BONE_DATABASE_lists/bone_cellType.Rdata"))
+# encode greek letters to UTF-8
+cellType_list = lapply(cellType_list, enc2utf8)
 usethis::use_data(cellType_list, overwrite = TRUE)
 
 # cellTypeSource_list
 load(url("https://github.com/kkupkova/Mouse-bone-markers/raw/main/BONE_DATABASE_lists/bone_cellType_and_sourceDB.Rdata"))
+# encode greek letters to UTF-8
+cellTypeSource_list = lapply(cellTypeSource_list, enc2utf8)
 usethis::use_data(cellTypeSource_list, overwrite = TRUE)
 
 # tissueSource_list
 load(url("https://github.com/kkupkova/Mouse-bone-markers/raw/main/BONE_DATABASE_lists/bone_tissue_and_sourceDB.Rdata"))
+# encode greek letters to UTF-8
+tissueSource_list = lapply(tissueSource_list, enc2utf8)
 usethis::use_data(tissueSource_list, overwrite = TRUE)
 
 # tissue_list
 load(url("https://github.com/kkupkova/Mouse-bone-markers/raw/main/BONE_DATABASE_lists/bone_tissue.Rdata"))
+# encode greek letters to UTF-8
+tissue_list = lapply(tissue_list, enc2utf8)
 usethis::use_data(tissue_list, overwrite = TRUE)
